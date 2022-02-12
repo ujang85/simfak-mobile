14d5ee34a7e12e0571e7bf15a6fa7fd64e4c39c0
ujang85 <root@192.168.0.103>
2022-02-12 20:32:55 +0700

    ubah header

--- views/layouts/left.php
+++ views/layouts/left.php
@@ -20,6 +20,7 @@ use yii\helpers\Html;
             'items' => [
                 ['label' => 'Dashboard', 'icon' => 'dashboard', 'url' => ['/']],
                 ['label' => 'Informasi', 'icon' => 'table_chart', 'url' => ""],
+                ['label' => 'Logout', 'url' => ['site/logout'],'data-method'=>'post'],
             ]
         ]); ?>    
     </div>
